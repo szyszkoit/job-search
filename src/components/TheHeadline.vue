93fafa5340c85467e4c7153008113ff0f61a03d2
kszyszko <krzysztof.szyszko-ext@hexagon.com>
2024-07-11 20:47:23 +0200

    Styling hero headline

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,7 +1,11 @@
 <template>
   <section>
-    <h1>{{ action }} for everyone</h1>
-    <h2>Find your next job at Cone Corp.</h2>
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span
+      ><br />
+      for everyone
+    </h1>
+    <h2 class="text-3xl font-light">Find your next job at Cone Corp.</h2>
   </section>
 </template>
 <script>
@@ -12,6 +16,13 @@ export default {
       action: "Build",
       interval: null,
       actions: ["Build", "Create", "Design", "Code"]
+    }
+  },
+  computed: {
+    actionClasses() {
+      return {
+        [this.action.toLocaleLowerCase()]: true
+      }
     }
   },
   created() {
@@ -31,3 +42,20 @@ export default {
   }
 }
 </script>
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+
+.create {
+  color: #34a863;
+}
+
+.design {
+  color: #f9ab00;
+}
+
+.code {
+  color: #d93025;
+}
+</style>
