9a9e0b0df5ee41536e9f985d93a12038766e62c3
kszyszko <krzysztof.szyszko-ext@hexagon.com>
2024-07-11 20:27:27 +0200

    Adding dynamic headline text

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <h1>Build for everyone</h1>
+    <h1>{{ action }} for everyone</h1>
     <h2>Find your next job at Cone Corp.</h2>
   </section>
 </template>
@@ -9,7 +9,24 @@ export default {
   name: "TheHeadline",
   data() {
     return {
-      sample: "Hello"
+      action: "Build",
+      interval: null,
+      actions: ["Build", "Create", "Design", "Code"]
+    }
+  },
+  created() {
+    this.changeTitle()
+  },
+  beforeUnmount() {
+    clearInterval(this.interval)
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const currentActionIndex = this.actions.indexOf(this.action) // 0
+        const nextActionIndex = (currentActionIndex + 1) % 4
+        this.action = this.actions[nextActionIndex]
+      }, 3000)
     }
   }
 }
