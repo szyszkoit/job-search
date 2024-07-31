11821965d55310b63bbfa3c9bfb0e04ed158afc9
kszyszko <krzysztof.szyszko-ext@hexagon.com>
2024-07-31 22:31:57 +0200

    Adding unit tests for Job Search Form

--- src/components/JobSearch/JobSearchForm.vue
+++ src/components/JobSearch/JobSearchForm.vue
@@ -6,15 +6,15 @@
     <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
-        <label class="absolute -top-10 left-0" for="">Role</label>
-        <text-input v-model="role" placeholder="Software engineer" />
+        <label class="absolute -top-10 left-0" for="role">Role</label>
+        <text-input id="role" v-model="role" placeholder="Software engineer" />
       </div>
       <span class="flex items-center border-l border-r border-brand-gray-3 bg-brand-gray-2 px-3">
         in
       </span>
       <div class="relative flex h-full flex-1 items-center pl-3">
-        <label class="absolute -top-10 left-0" for="">Where ?</label>
-        <text-input v-model="location" placeholder="Los Angeles" />
+        <label class="absolute -top-10 left-0" for="location">Where ?</label>
+        <text-input id="location" v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
